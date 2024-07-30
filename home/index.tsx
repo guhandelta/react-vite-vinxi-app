ae0642901df004c47644321c0b9ec0f757a7dc83
Guhaprasaanth Nandagopal <guhan.delta@gmail.com>
2024-07-30 12:25:11 -0400

    Added Tailwind to the app with

--- home/index.tsx
+++ home/index.tsx
@@ -1,6 +1,11 @@
 /// <reference types="vinxi/types/client" />
 import ReactDOM from 'react-dom/client';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <h1>Hello, world!</h1>
-);
+const rootElement = document.getElementById('root');
+// To handle `Returns a reference to the first object with the specified value of the ID attribute.` warning
+if (rootElement) {
+    ReactDOM.createRoot(rootElement).render(
+        <h1 className='text-center text-5xl'>Hello, world!</h1>
+    );
+}
